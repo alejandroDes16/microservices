1f3a0b33c153587058184d1c43a21c90f5272761
alejandroDes16 <alejandroalfonsotalavera@gmail.com>
2022-11-14 23:32:42 +0100

    auth-jwt

---
+++ auth-service/src/main/java/com/microservicios/authservice/dto/AuthUserDto.java
@@ -0,0 +1,17 @@
+package com.microservicios.authservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthUserDto {
+
+    private String userName;
+    private String password;
+
+}
