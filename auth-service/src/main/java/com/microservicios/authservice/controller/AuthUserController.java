025ffbc0a5dcace2c22d8ab0b915a50f51830878
alejandroDes16 <alejandroalfonsotalavera@gmail.com>
2022-11-15 16:09:18 +0100

    auth-jwt

---
+++ auth-service/src/main/java/com/microservicios/authservice/controller/AuthUserController.java
@@ -0,0 +1,44 @@
+package com.microservicios.authservice.controller;
+
+import com.microservicios.authservice.dto.AuthUserDto;
+import com.microservicios.authservice.dto.TokenDto;
+import com.microservicios.authservice.entity.AuthUser;
+import com.microservicios.authservice.service.AuthUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.ws.rs.POST;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthUserController {
+
+    @Autowired
+    AuthUserService authUserService;
+
+    public ResponseEntity<TokenDto> login(@RequestBody AuthUserDto dto){
+        TokenDto tokenDto = authUserService.login(dto);
+        if(tokenDto == null){
+            return  ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(tokenDto);
+    }
+
+    @PostMapping("/validate")
+    public ResponseEntity<TokenDto> validate(@RequestParam String token){
+        TokenDto tokenDto = authUserService.validate(token);
+        if(tokenDto == null){
+            return  ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(tokenDto);
+    }
+    @PostMapping("/create")
+    public ResponseEntity<AuthUser> create(@RequestBody AuthUserDto dto){
+        AuthUser authUser = authUserService.save(dto);
+        if(authUser == null){
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(authUser);
+    }
+}
