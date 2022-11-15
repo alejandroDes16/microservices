d6722c007541e338e112cdf53ba7ea8830e5df8d
alejandroDes16 <alejandroalfonsotalavera@gmail.com>
2022-11-15 16:51:59 +0100

    auth-jwt

--- gateway-service/src/main/java/com/microservicios/gatewayservice/config/AuthFilter.java
+++ gateway-service/src/main/java/com/microservicios/gatewayservice/config/AuthFilter.java
@@ -24,12 +24,12 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
     @Override
     public GatewayFilter apply(Config config) {
         return (((exchange, chain) -> {
-            if(exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)){
+            if(!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)){
                 return onError(exchange, HttpStatus.BAD_REQUEST);
             }
             String tokenHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
             String [] chunks = tokenHeader.split(" ");
-            if(chunks.length != 2 || chunks[0].equals("Bearer")){
+            if(chunks.length != 2 || !chunks[0].equals("Bearer")){
                 return onError(exchange, HttpStatus.BAD_REQUEST);
             }
             return webClient.build()
